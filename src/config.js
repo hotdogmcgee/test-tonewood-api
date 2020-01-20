b21a840406e44be95f57f810bccde5a3ccfb3bb3
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2020-01-20 15:14:09 -0500

    more tests, issues with locally run server

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://woody@localhost/tonewoods_thinkfultest',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://woody:allthewood52@localhost/tonewoods',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://woody@localhost/tonewoods-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   }
