e035ae02c0b8bf38d9d0d294af33c3a1cc084f1e
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2022-03-08 10:34:07 -0800

    further ssl mod

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const { PORT, DATABASE_URL } = require('./config.js')
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL,
+  connection: DATABASE_URL + "?ssl=true",
   ssl: {
     rejectUnauthorized: false
   }
