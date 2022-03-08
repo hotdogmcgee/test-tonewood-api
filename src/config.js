1b6d23d71701815414c5178857b4cb2823e27034
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2022-03-08 10:50:32 -0800

    update server with excplicit connection vars

--- src/config.js
+++ src/config.js
@@ -4,4 +4,10 @@ module.exports = {
     DATABASE_URL: process.env.DATABASE_URL,
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://woody@localhost/tonewoods-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+
+    DB_NAME:process.env.PRODUCTION_MIGRATION_DB_NAME,
+    DB_HOST:process.env.PRODUCTION_MIGRATION_DB_HOST,
+    DB_PORT:process.env.PRODUCTION_MIGRATION_DB_PORT,
+    DB_USER:process.env.PRODUCTION_MIGRATION_USER,
+    DB_PASSWORD:process.env.PRODUCTION_MIGRATION_PASSWORD
   }
--- src/server.js
+++ src/server.js
@@ -1,17 +1,29 @@
-const knex = require('knex')
-const app = require('./app')
-const { PORT, DATABASE_URL } = require('./config.js')
+const knex = require("knex");
+const app = require("./app");
+const {
+  PORT,
+  DATABASE_URL,
+  DB_NAME,
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+} = require("./config.js");
 
 const db = knex({
-  client: 'pg',
-  connection: DATABASE_URL + "?ssl=true",
-  ssl: {
-    rejectUnauthorized: false
-  }
-})
+  client: "pg",
+  connection: {
+    host: DB_HOST,
+    name: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    ssl: true,
+  },
+});
 
-app.set('db', db)
+app.set("db", db);
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
